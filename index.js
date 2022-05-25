d1a9d9254e8dd896c8f53496e0ab65a412839cd0
AndrewTCarter <Andrewkarter@gmail.com>
2022-05-25 13:55:36 -0400

    commit tree 9fcccd3d98966355b08a0a9a90a506105405e0bc

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(name) {
+    let cards = [];
+    
+    for(let i = 0; i < name.length; i++) {
+        cards.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    };
+    return cards
+}
+
+function countDown(num) {
+    while(num >= 0) {
+        console.log(num);
+        num --;
+    }
+}
